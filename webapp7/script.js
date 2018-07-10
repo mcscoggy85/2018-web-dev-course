a24d210954f18c6856b329d535c3d1d8b2fe5349
mrcoggsworth <R0ast3d:Duck$:cant:f1y>
2018-07-10 08:32:43 -0500

    (update) starting dom manipulation

--- webapp7/script.js
+++ webapp7/script.js
@@ -1,1 +1,13 @@
-const css = document.querySelector('h3');
+const css = document.querySelector('#h3-title');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+let body = document.getElementById('gradient');
+
+function setGradient() {
+    body.style.background =
+        `Linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    css.textContent = `${body.style.background};`;
+}
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
