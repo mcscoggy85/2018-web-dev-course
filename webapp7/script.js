0c2140c340df0c98a3793623d8c51da2b507bcdd
mrcoggsworth <R0ast3d:Duck$:cant:f1y>
2018-07-10 09:20:39 -0500

    make gradient web app to change bg-color to a gradient, added input buttons to change manually as well as added in a random generator button.

--- webapp7/script.js
+++ webapp7/script.js
@@ -1,13 +1,26 @@
 const css = document.querySelector('#h3-title');
-const color1 = document.querySelector('.color1');
-const color2 = document.querySelector('.color2');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
 let body = document.getElementById('gradient');
+const random = document.querySelector('.color-btn');
 
 function setGradient() {
     body.style.background =
         `Linear-gradient(to right, ${color1.value}, ${color2.value})`;
     css.textContent = `${body.style.background};`;
 }
+
+function randomColors(){
+    let randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+    let randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+    color1.value = `#${randomColor1}`;
+    color2.value = `#${randomColor2}`;
+    setGradient();
+}
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
+
+body.onload = setGradient;
+
+random.addEventListener('click', randomColors);
