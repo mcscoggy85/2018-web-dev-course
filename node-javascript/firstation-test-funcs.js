7bfb099e5b61267e833675f6ffcf4f41fbe04fd4
mcscoggy85 <cscogin@wgu.edu>
2018-08-03 17:07:26 -0500

    updates to side prj

--- node-javascript/firstation-test-funcs.js
+++ node-javascript/firstation-test-funcs.js
@@ -1,27 +1,68 @@
 const fs = require('fs-extra');
 
 const status = [{
-  stats: {
-    vpnStatus: {},
-    on: null,
+  sysd: {
+    vpnStatus: {
+        build: {},
+        on: null,
+
+    },
+    deviceStatus: {
+
+    }
   },
+  logs: {
+      openvpn: {
+          stats: {},
+          logs: {}
+      },
+      firehole: {
+          iptables: {}
+      }
+  }
 }];
 
-const readFiles = async(file) => {
-    const response = await fs.readFile(file);
-    const data = await response.toString();
-    status[0].stats.vpnStatus = data;
-    status[0].stats.on = data.includes('Manjaro');
-  }
+// const readFiles = async(file) => {
+//     const response = await fs.readFile(file);
+//     const data = await response.toString();
+//     status[0].stats.vpnStatus = data;
+//     status[0].stats.on = data.includes('Manjaro');
+//   }
 
-const setVPN = async () => {
-  await readFiles('/etc/issue');
-  console.log(status);
-  console.log(status[0].stats.vpnStatus);
+const setVPN = async (file) => {
+  const response = await fs.readFile(file);
+  const data = await response.toString();
+  status[0].sysd.vpnStatus.build = data;
+  status[0].sysd.vpnStatus.on = data.includes('Linux Mint');
 };
+
+const vpnLogs = async(file) => {
+  const response = await fs.readFile(file);
+  const data = await response.toString();
+  status[0].logs.openvpn.stats = data
+//   console.log(status[0].logs.openvpn.stats);
+
+}
 
 // (async () => {
 //     setVPN();
 // })();
 
-setVPN();
+const readObj = async() => {
+    let ctr = 0;
+    await console.log('Overall Obj:', status[0]);
+    const statusVpn = status[0].logs.openvpn.stats.split('\n');
+    statusVpn.forEach(element => {
+        console.log(ctr +' ===== '+ element);
+        ctr++
+    });
+
+}
+
+const constructObj = async() => {
+    await setVPN('/etc/issue');
+    await vpnLogs('./openvpn-status.log');
+    await readObj();
+}
+
+constructObj();
